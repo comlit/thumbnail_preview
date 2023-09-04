b4df88352d3fde226acc0ed253b366802aea9f08
Fabian <1803fabian@gmail.com>
2023-09-04 20:26:19 +0200

    minimum viable shit

--- contentScript.js
+++ contentScript.js
@@ -1,35 +1,49 @@
-let links = document.querySelectorAll('a');
-//filter links to only include ones with youtube in the href
-links = Array.from(links).filter(link => link.href.includes('youtube') || link.href.includes('youtu.be'));
-//add a purple background to each link
+let links = Array.from(document.querySelectorAll('a[href*="youtube."]:not(:has(.hoverable)), a[href*="youtu.be"]:not(:has(.hoverable))'));
 
-links.forEach(link => {
-    //extract the video id from the link
-    let videoId = link.href.split('v=')[1];
-    //if the link is a youtu.be link, extract the video id from the link
-    if (!videoId) {
-        videoId = link.href.split('youtu.be/')[1];
-    }
-    if (!videoId)
-        return
-    videoId = videoId.split('&')[0];
-    console.log(videoId);
-    link.innerHTML = `<div class="hoverable"> <span class="hoverable__main">${link.innerHTML}</span><span class="hoverable__tooltip"><img src="https://img.youtube.com/vi/${videoId}/0.jpg" alt="Description of the image"></span> </div>`;
+links.forEach(node => {
+    addOverlay(node);
 });
 
 const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
         if (mutation.addedNodes.length > 0) {
-            // Handle added nodes here
-            let new_nodes = Array.from(mutation.addedNodes);
-            new_nodes = new_nodes.filter(node => node.tagName === 'A');
+            let new_nodes = Array.from(document.querySelectorAll('a[href*="youtube."]:not(:has(.hoverable)), a[href*="youtu.be"]:not(:has(.hoverable))'));
             for (let node of new_nodes) {
-
+                addOverlay(node);
             }
+
+            let newn = Array.from(document.querySelectorAll('a:not(:has(.hoverable))'));
+            for (let node of newn) {
+                node.innerHTML = `<div class="hoverable"> <span class="hoverable__main">${node.innerHTML}</span><span class="hoverable__tooltip"><img src="https://i.ytimg.com/vi/2QSPk9uIfyI/maxresdefault.jpg" alt="Description of the image"> <div class="overlay-text"> ajaajajajjaj </div> </span> </div>`;
+            }
+
         }
 
     });
 });
+
+function parseLink(link) {
+    //extract the video id from the link
+    let videoId = link.split('v=')[1];
+    //if the link is a youtu.be link, extract the video id from the link
+    if (!videoId) {
+        videoId = link.split('youtu.be/')[1];
+    }
+    if (!videoId)
+        return undefined
+    videoId = videoId.split('&')[0];
+    videoId = videoId.split('?')[0];
+    return videoId;
+}
+
+async function addOverlay(node) {
+    let videoId = parseLink(node.href);
+    if (!videoId)
+        return;
+    let data = await fetch(`https://youtube.com/oembed?url=${node.href}&format=json`).then((response) => response.json())
+
+    node.innerHTML = `<div class="hoverable"> <span class="hoverable__main">${node.innerHTML}</span><span class="hoverable__tooltip"><img src="https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg" alt="Description of the image"> <div class="overlay-text"> ${data.title} </div> </span> </div>`;
+}
 
 // Start observing the document
 observer.observe(document, { childList: true, subtree: true });
