678aa905c920303fef84e5d8d3143f454e9663d4
Fabian <1803fabian@gmail.com>
2023-09-27 21:46:40 +0200

    added support for ig profiles

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,19 @@
-let links = Array.from(document.querySelectorAll('a[href*="tiktok."]:not(:has(.hoverable)), a[href*="youtube."]:not(:has(.hoverable)), a[href*="youtu.be"]:not(:has(.hoverable))'));
+let selector = "a[href*='instagram.']:not(:has(.hoverable)), a[href*='tiktok.']:not(:has(.hoverable)), a[href*='youtube.']:not(:has(.hoverable)), a[href*='youtu.be']:not(:has(.hoverable))"
+
+let links = Array.from(document.querySelectorAll(selector));
 
 //links = document.querySelectorAll('a')
 
 
 links.forEach(node => {
-    addlister(node)
+    addmarker(node);
+    addlister(node);
 });
 
 const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
         if (mutation.addedNodes.length > 0) {
-            let new_nodes = Array.from(document.querySelectorAll('a[href*="tiktok."]:not(:has(.hoverable)), a[href*="youtube."]:not(:has(.hoverable)), a[href*="youtu.be"]:not(:has(.hoverable))'));
+            let new_nodes = Array.from(document.querySelectorAll(selector));
 
             //new_nodes = Array.from(document.querySelectorAll('a'))
 
@@ -34,66 +37,49 @@ div.className = 'move';
 
 document.body.appendChild(div);
 
-let elem = document.querySelector('.mover'),
-    over = false
-    tt = false;
+let elem = document.querySelector('.mover')
+let over = false
+let id = 187
+let dimens = { width: 80, height: 40 }
+let pos = { x: 0, y: 0 }
 
 
 // element mousemove to stop 
 document.body.addEventListener('mousemove', function (e) {
     if (over) {
-        let height = div.querySelector('img')?.offsetHeight ?? 235;
-
-        //weird bug fix
-        if (height == 10)
-            height = 235
-
-        let left = e.pageX + 5
-        let top = e.pageY + 15
-
-        //left = Math.max((e.pageX - div.offsetWidth / 1.3), 10)
-        //top = Math.min(e.pageY + 10, window.innerHeight - height - 10)
-
-        if (left > window.innerWidth - div.offsetWidth - 15) {
-            //right
-            left = window.innerWidth - div.offsetWidth - 15
-        }
-
-        if (top > window.innerHeight - height - 15) {
-            //bottom
-            top = e.pageY - height - 15
-        }
-        div.style.left = `${left}px`;
-        div.style.top = `${top}px`;
+        pos.x = e.pageX;
+        pos.y = e.pageY;
+        positionOverlay()
     }
 }, true);
 
-//addOverlay()
+function positionOverlay() {
+    let left = pos.x + 5
+    let top = pos.y + 15
 
+    if (left > window.innerWidth - dimens.width - 15) {
+        //right
+        left = window.innerWidth - dimens.width - 15
+    }
 
-function addlister(test) {
-    test.addEventListener("mouseleave", function (event) {
+    if (top > window.innerHeight - dimens.height - 15) {
+        //bottom
+        top = pos.y - dimens.height - 15
+    }
+    div.style.left = `${left}px`;
+    div.style.top = `${top}px`;
+}
+
+function addlister(element) {
+    element.addEventListener("mouseleave", function (event) {
         over = false;
         removeOverlay()
     }, false);
-    test.addEventListener("mouseover", function (event) {
-        addOverlay(test)
+    element.addEventListener("mouseover", function (event) {
         over = true;
+        id = Math.random() * 10000
+        addOverlay(element, id)
     }, false);
-}
-
-function parseLink(link) {
-    //extract the video id from the link
-    let videoId = link.split('v=')[1];
-    //if the link is a youtu.be link, extract the video id from the link
-    if (!videoId) {
-        videoId = link.split('youtu.be/')[1];
-    }
-    if (!videoId)
-        return undefined
-    videoId = videoId.split('&')[0];
-    videoId = videoId.split('?')[0];
-    return videoId;
 }
 
 function addmarker(node) {
@@ -101,31 +87,117 @@ function addmarker(node) {
     node.setAttribute('data-hoverable', 'true');
 }
 
-async function addOverlay(elem) {
+async function addOverlay(elem, pid) {
     let href = elem.href;
-    if (href.includes('tiktok.')) {
-        tt = true;
-        let data = await fetch(`https://www.tiktok.com/oembed?url=${href}`).then((response) => response.json())
-        
-        //check for error code
-        if (data?.code == 400)
-            return;
-        if(!data.thumbnail_url || !data.title)
-            return;
+    let parsed = parselinks(href);
 
-        if (over)
-            div.innerHTML = `<div> <img src="${data.thumbnail_url}" alt="Youtube Video" class=hoverimage  style="width: 200px;"> <div class="overlay-text" style="width: 170px;"> ${data.title} </div> </div>`;
-    }
-    else {
-        tt = false;
-        let videoId = parseLink(href);
-        if (!videoId)
-            return;
-        let data = await fetch(`https://youtube.com/oembed?url=${href}&format=json`).then((response) => response.json())
+    //just here for testing should be below the parsed check
+    //show loading animation
 
-        if (over)
-            div.innerHTML = `<div> <img src="https://i.ytimg.com/vi/${videoId}/hqdefault.jpg" alt="Description of the image" class=hoverimage> <div class="overlay-text"> ${data.title} </div> </div>`;
+
+    if (!parsed)
+        return;
+
+    div.innerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>'
+    dimens = { width: 80, height: 40 }
+
+    switch (parsed.type) {
+        case "yt":
+            await showyt(parsed, pid);
+            break;
+        case "tt":
+            await showtt(parsed, pid);
+            break;
+        case "ig":
+            showig(parsed, pid);
+            break;
     }
+}
+
+function parselinks(link) {
+    let ytregex = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/; //maybe add support for yt profiles later
+    let ttregex = /^.*(tiktok\.com\/)([^#\&\?]*).*/; //diferentiate between profile and video
+    let igprofileregex = /instagram\.com\/\w*\/$/;
+
+    let ytmatch = link.match(ytregex);
+    let ttmatch = link.match(ttregex);
+    let igprofilematch = link.match(igprofileregex);
+
+    if (!ytmatch && !ttmatch && !igprofilematch)
+        return undefined;
+
+    if (ytmatch)
+        return { type: "yt", data: ytmatch?.[2] };
+    if (ttmatch)
+        return { type: "tt", data: link };
+    if (igprofilematch)
+        return { type: "ig", data: link + "embed/" };
+}
+
+async function showyt(data, pid) {
+    let videoId = data.data;
+    let ytdata = await fetch(`https://youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}&format=json`).then((response) => response.json())
+
+    if (over && id == pid)
+        div.innerHTML = `<div> <img src="https://img.youtube.com/vi/${videoId}/maxresdefault.jpg" alt="Youtube Video" class=hoverimage> <div class="overlay-text"> ${ytdata.title} </div> </div>`;
+
+    changedimensions(pid)
+}
+
+async function showtt(data, pid) {
+    let ttdata;
+    try {
+        ttdata = await fetch(`https://www.tiktok.com/oembed?url=${data.data}`).then((response) => response.json())
+    }
+    catch (e) {
+        console.log(e)
+        showerror(pid)
+        return;
+    }
+    if (data?.code == 400) {
+        showerror(pid)
+        return;
+    }
+    //diferentiate between profile and video
+
+
+    if (over && id == pid)
+        div.innerHTML = `<div> <img src="${ttdata.thumbnail_url}" alt="Tiktok Video" class=hoverimage  style="width: 200px;"> <div class="overlay-text" style="width: 170px;"> ${ttdata.title} </div> </div>`;
+
+    changedimensions(pid)
+}
+
+function showig(data, pid) {
+    if (over && id == pid) {
+        div.innerHTML = `<iframe src="${data.data}" width="100%" height="100%" frameborder="0" scrolling="no" allowtransparency="true"></iframe>`
+        dimens.width = div.offsetWidth;
+        dimens.height = div.offsetHeight;
+        positionOverlay()
+    }
+
+}
+
+function changedimensions(pid) {
+    let img = document.querySelector('.hoverimage')
+    if (!img)
+        return;
+    img.addEventListener('load', function () {
+        if (id = pid) {
+            dimens.width = div.offsetWidth;
+            dimens.height = div.offsetHeight;
+            positionOverlay()
+        }
+    })
+}
+
+/**
+ * shows Error message in case of an error
+ * @param {*} pid 
+ */
+async function showerror(pid) {
+    if (over && id == pid)
+        div.innerHTML = `<div class="errordiv"> ERROR</div>`;
+
 }
 
 function removeOverlay() {
